d3d85215fba45534ad66bea5bd3b3f77286d9bc7
Lagutin-Ruslan <70953657+Lagutin-Ruslan@users.noreply.github.com>
2021-03-20 20:31:11 +0300

    Доработал проект для защиты

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-import './api.js';
-import './filter.js';
-import './form.js';
-import './images-preview.js';
 import './map.js';
-import './message.js';
+import './form.js';
+import './filter.js';
 import './validator.js';
+import './images-preview.js';
+import './message.js';
+import './api.js';
